fd3fc7e526f1c3fd3cda1e0327bf8ca575d4f798
Dhruv Gupta <125383912+dhruvgupta114@users.noreply.github.com>
2024-05-09 23:56:33 +0530

    Add files via upload

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import React from "react"
+import Navbar from './Navbar'
+
+
+function App() {
+
+  return (
+    <>
+       <Navbar />
+      <div className="bg-indigo-600 w-full h-screen"></div>
+    </>
+  )
+}
+
+export default App
